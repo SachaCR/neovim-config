0d17606b12243a9eac24f37d7a017428e102675b
Sacha Clerc-Renaud <sachacr@gmail.com>
2025-09-29 15:39:28 +0200

    add some vim options

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -9,3 +9,18 @@ vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
 vim.keymap.set('i', '<C-space>', function()
   vim.lsp.completion.get()
 end)  
+
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+vim.opt.smartindent = true
+vim.opt.wrap = false
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+vim.opt.termguicolors = true
+vim.opt.scrolloff = 8
+vim.opt.colorcolumn = "80"
