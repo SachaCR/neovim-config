645077a78f8ba0f43a3ad131ccb41754f8e900f0
Sacha Clerc-Renaud <sachacr@gmail.com>
2025-10-07 10:07:38 +0200

    Update neovim config for telescope and Rust

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,9 +2,19 @@ return {
 	'nvim-telescope/telescope.nvim', tag = '0.1.8',
 	dependencies = { 'nvim-lua/plenary.nvim' },
 	config = function () 
-      		local builtin = require("telescope.builtin")
-      		vim.keymap.set('n', "<leader>p", builtin.git_files, {})
-      		vim.keymap.set('n', "<leader>f", builtin.find_files, {})
-    	end
+    require("telescope").setup({
+      defaults = {
+        file_ignore_patterns = {}, -- optional extra ignore patterns
+      },
+      pickers = {
+        find_files = {
+          find_command = { 'fd', '--type', 'f', '--hidden', '--strip-cwd-prefix' },
+        },
+      },
+    })
+
+    vim.keymap.set('n', "<leader>p", "<cmd>Telescope git_files<cr>", {})
+    vim.keymap.set('n', "<leader>f", "<cmd>Telescope find_files<cr>", {})
+  end
 	
 }
